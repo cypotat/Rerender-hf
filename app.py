a9c20260496ac4e01d4607f1809086d1c9991058
Cyber Potato <33260586+cypotat@users.noreply.github.com>
2023-06-28 13:05:12 +0300

    Update app.py fix

--- app.py
+++ app.py
@@ -938,4 +938,4 @@ with block:
     run_button3.click(fn=process3, outputs=[result_keyframe])
 
 block.queue(concurrency_count=1, max_size=20)
-block.launch(server_name='0.0.0.0')
+block.launch(share=True)
